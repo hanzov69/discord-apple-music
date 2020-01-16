6c0e0eb61bfcfd126256a9ba8ed71af923c2fa59
hanzov69 <hanzo@freezerpants.com>
2020-01-16 12:40:38 -0500

    Update discord-apple-music.py

--- discord-apple-music.py
+++ discord-apple-music.py
@@ -2,16 +2,14 @@ from pypresence import Presence
 import time
 import applescript
 
-client_id = '667394345279684626'  # Fake ID, put your real one here
+client_id = '667394345279684626'  # This is mine, I think it's reusable by all?
 RPC = Presence(client_id)  # Initialize the client class
 RPC.connect() # Start the handshake loop
 
 current_song = applescript.run('getsong.scpt')
-#print(RPC.update(state="Playing", details=current_song.out))  # Set the presence
-print(RPC.update(state=current_song.out))
+print(RPC.update(state=current_song.out)) # initial presence
 
 while True:  # The presence will stay on as long as the program is running
     time.sleep(15) # Can only update rich presence every 15 seconds
     current_song = applescript.run('getsong.scpt')
-    #print(RPC.update(state="Playing", details=current_song.out))  # Set the presence
-    print(RPC.update(state=current_song.out))
+    print(RPC.update(state=current_song.out)) # updated presence
